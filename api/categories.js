bf596b8d799ed52f6b5086071954283795d8c16a
VChet <17050347+VChet@users.noreply.github.com>
2019-02-01 12:59:15 +0500

    Populate elements and categories requests

--- api/categories.js
+++ api/categories.js
@@ -5,7 +5,23 @@ module.exports = {
 const { Category } = require("../models/category");
 
 function getCategories(req, res) {
-    Category.find({}, (err, categories) => {
+    const pipeline = [
+        { $lookup: {
+            from: "elements",
+            localField: "_id",
+            foreignField: "category",
+            as: "elements"
+        }},
+        { $project: {
+            __v: 0,
+            elements: {
+                category: 0,
+                __v: 0
+            }
+        }}
+    ];
+    Category.aggregate(pipeline).exec((err, categories) => {
+        if (err) return res.status(500).json({err});
         res.status(200).json({response: categories});
     });
 }
--- api/elements.js
+++ api/elements.js
@@ -5,7 +5,21 @@ module.exports = {
 const { Element } = require("../models/element");
 
 function getElements(req, res) {
-    Element.find({}, (err, elements) => {
-        res.status(200).json({response: elements});
+    const pipeline = [
+        { $lookup: {
+            from: "categories",
+            localField: "category",
+            foreignField: "_id",
+            as: "category"
+        }},
+        { $unwind: "$category" },
+        { $project: {
+            name: 1,
+            category: "$category.name"
+        }}
+    ];
+    Element.aggregate(pipeline).exec((err, elements) => {
+        if (err) return res.status(500).json({err});
+        return res.status(200).json({response: elements});
     });
 }
