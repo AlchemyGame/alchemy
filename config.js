fb64b655976c61615b8598c22941b6bbebbc35aa
VChet <17050347+VChet@users.noreply.github.com>
2019-03-08 23:04:18 +0500

    Add session config

--- config.js
+++ config.js
@@ -2,12 +2,18 @@ console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
 
 let overwriteConfig = require("./config.dev");
 
-if (process.env.NODE_ENV === "test") {
-  overwriteConfig = require("./config.test");
-}
+if (process.env.NODE_ENV === "test") overwriteConfig = require("./config.test");
 
 module.exports = {
   appPort: "7540",
   mongoUrl: "mongodb://localhost/alchemy",
+  appSalt: "SALT",
+  session: {
+    secret: "SECRET",
+    name: "alchemy.sid",
+    proxy: true,
+    resave: true,
+    saveUninitialized: false
+  },
   ...overwriteConfig
 };
