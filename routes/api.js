3e32ab493eb7dc8437f495cfcfb12322308a3440
VChet <17050347+VChet@users.noreply.github.com>
2019-03-08 23:14:34 +0500

    Add router middleware to check if user is authenticated

--- routes/api.js
+++ routes/api.js
@@ -22,19 +22,29 @@ const {
   deleteRecipe
 } = require("../api/recipes");
 
-router.get("/elements", getElements);
-router.post("/element/add", addElement);
-router.put("/element/update", updateElement);
-router.delete("/element/delete", deleteElement);
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.status(401).json({ error: "Authentication is required to perform this action" });
+}
 
-router.get("/categories", getCategories);
-router.post("/category/add", addCategory);
-router.put("/category/update", updateCategory);
-router.delete("/category/delete", deleteCategory);
+function isAdmin(req, res, next) {
+  if (req.user.role === "Admin") return next();
+  res.status(403).json({ error: "You are not authorized to perform this action" });
+}
 
-router.get("/recipes", getRecipes);
-router.post("/recipe/add", addRecipe);
-router.put("/recipe/update", updateRecipe);
-router.delete("/recipe/delete", deleteRecipe);
+router.get("/elements", isAuthenticated, getElements);
+router.post("/element/add", isAuthenticated, isAdmin, addElement);
+router.put("/element/update", isAuthenticated, isAdmin, updateElement);
+router.delete("/element/delete", isAuthenticated, isAdmin, deleteElement);
+
+router.get("/categories", isAuthenticated, getCategories);
+router.post("/category/add", isAuthenticated, isAdmin, addCategory);
+router.put("/category/update", isAuthenticated, isAdmin, updateCategory);
+router.delete("/category/delete", isAuthenticated, isAdmin, deleteCategory);
+
+router.get("/recipes", isAuthenticated, getRecipes);
+router.post("/recipe/add", isAuthenticated, isAdmin, addRecipe);
+router.put("/recipe/update", isAuthenticated, isAdmin, updateRecipe);
+router.delete("/recipe/delete", isAuthenticated, isAdmin, deleteRecipe);
 
 module.exports = router;
