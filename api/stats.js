16f05a48bdfd311cdb4620c5528c164a3832b7c6
VChet <17050347+VChet@users.noreply.github.com>
2019-04-09 16:06:52 +0500

    Add active and banned users counters

--- api/stats.js
+++ api/stats.js
@@ -2,15 +2,27 @@ module.exports = {
   getStats
 };
 
+const moment = require("moment");
+
 const { Element } = require("../models/element");
 const { User } = require("../models/user");
 
 async function getStats(req, res, next) {
   const elements = await Element.estimatedDocumentCount();
   const users = await User.countDocuments({ isDisabled: false });
+  const activeUsers = await User.countDocuments({
+    isDisabled: false,
+    lastEntered: {
+      $gt: moment().startOf("month"),
+      $lt: moment().endOf("month")
+    }
+  });
+  const bannedUsers = await User.countDocuments({ isDisabled: true });
 
   res.json({
     usersCount: users,
+    activeUsersCount: activeUsers,
+    bannedUsersCount: bannedUsers,
     // discoveredElementsCount: discoveredElements
     elementsCount: elements
   });
