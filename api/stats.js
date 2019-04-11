d2015fb91728f0656ec228a54353e0c092f2063d
VChet <17050347+VChet@users.noreply.github.com>
2019-04-11 12:17:48 +0500

    Add undiscovered elements counter

--- api/stats.js
+++ api/stats.js
@@ -8,22 +8,29 @@ const { Element } = require("../models/element");
 const { User } = require("../models/user");
 
 async function getStats(req, res, next) {
-  const elements = await Element.estimatedDocumentCount();
-  const users = await User.countDocuments({ isDisabled: false });
-  const activeUsers = await User.countDocuments({
+  const usersCount = await User.countDocuments({ isDisabled: false });
+  const activeUsersCount = await User.countDocuments({
     isDisabled: false,
     lastEntered: {
       $gt: moment().startOf("month"),
       $lt: moment().endOf("month")
     }
   });
-  const bannedUsers = await User.countDocuments({ isDisabled: true });
+  const bannedUsersCount = await User.countDocuments({ isDisabled: true });
+  const elementsCount = await Element.estimatedDocumentCount();
+
+  const users = await User.find({}).lean();
+  let discoveredElementsSet = new Set();
+  users.map(user => {
+    user.elements.map(el => discoveredElementsSet.add(el.toString()));
+  });
+  const undiscoveredElementsCount = elementsCount - discoveredElementsSet.size;
 
   res.status(200).json({
-    usersCount: users,
-    activeUsersCount: activeUsers,
-    bannedUsersCount: bannedUsers,
-    // discoveredElementsCount: discoveredElements
-    elementsCount: elements
+    usersCount,
+    activeUsersCount,
+    bannedUsersCount,
+    elementsCount,
+    undiscoveredElementsCount
   });
 }
