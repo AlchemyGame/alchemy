aba28d34d086f0c1aa720716d90d1c0da1de6154
VChet <17050347+VChet@users.noreply.github.com>
2019-04-16 20:23:15 +0500

    Send user data after registration

--- api/mail.js
+++ api/mail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 const config = require("../config");
 const transporter = nodemailer.createTransport(config.nodemailer);
 
-const sendEmail = (to, subject, html, res) => {
+const sendEmail = (to, subject, html, res, userData = null) => {
   const data = {
     from: "Alchemy <noreply@alchemy.game>",
     to,
@@ -10,15 +10,16 @@ const sendEmail = (to, subject, html, res) => {
     html
   };
   transporter.sendMail(data, (error, info) => {
+    const response = {};
+    if (userData) response.user = userData;
     if (error) {
-      if (error.code === "EAUTH") {
-        console.log("MAILER: Wrong credentials");
-        return res.status(500).json({ error: "Data has been updated but mailer is not available" });
-      }
-      return res.status(500).json({ error });
+      console.log({ error });
+      response.error = "Server completed the request, but mailer is not available";
+    } else {
+      console.log({ info });
+      response.mailer = "Email has been sent";
     }
-    console.log({ info });
-    return res.status(200).json({ response: "Data has been updated and email has been sent" });
+    return res.status(200).json(response);
   });
 };
 
--- api/users.js
+++ api/users.js
@@ -103,7 +103,7 @@ function createAccount(req, res) {
     userData.password = Math.random().toString(36).slice(2);
   }
   userData.role = "User";
-  User.createNew(userData, error => {
+  User.createNew(userData, (error, user) => {
     if (error) return res.status(409).json({ error });
     let html = "";
     html += "You are registered in Alchemy.<br/><br/>";
@@ -112,7 +112,7 @@ function createAccount(req, res) {
     html += `Password: ${userData.password}<br/><br/>`;
     html += `<a href="${req.protocol}://${req.get("host")}/login">Alchemy</a>`;
     if (process.env.NODE_ENV === "test") return res.json({ html });
-    sendEmail(req.body.email, "Account registration", html, res);
+    sendEmail(req.body.email, "Account registration", html, res, user);
   });
 }
 
